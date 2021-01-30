fbacf146d5f7b9275922bfe48fb922323f727502
vinod <vinodkumarmahato1@gmail.com>
2021-01-30 11:10:41 +0530

    Flex Pannels

---
+++ index.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+console.log(panels)
+function toggleOpen(){
+ 
+    this.classList.toggle("open")
+}
+panels.forEach( panel => panel.addEventListener("click", toggleOpen));
+panels.forEach( panel => panel.addEventListener("transitionend", toggleActive));
+
+function toggleActive(e){
+    if(e.propertyName.includes("flex")){
+        this.classList.toggle("open-active");
+    }
+  
+};
